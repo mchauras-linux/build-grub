8cb9753d487ce4e2d190b5808bcb5316a5e09c84
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-11-07 19:50:09 +0530

    Updated
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+# Check if at least one argument is provided
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <branch name>"
+    exit -1
+fi
 
-docker build --build-arg branch=retry --tag suse:latest . --network host
+docker build --build-arg branch=$1 --tag suse:latest . --network host
 docker run -it --network host suse
